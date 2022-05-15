ee07e11240161e9f61be6614da0d37b133529ec1
David Pecot <4504352+northernocean@users.noreply.github.com>
2022-05-15 15:59:13 -0400

    fix errors in models

--- store/models.py
+++ store/models.py
@@ -37,7 +37,7 @@ class Order(models.Model):
     PAYMENT_STATUS_PENDING='P'
     PAYMENT_STATUS_COMPLETE='C'
     PAYMENT_STATUS_FAILED='F'
-    PAYMENT_STATUS_OPTIONS=[PAYMENT_STATUS_PENDING, PAYMENT_STATUS_COMPLETE, PAYMENT_STATUS_FAILED]
+    PAYMENT_STATUS_OPTIONS=[(PAYMENT_STATUS_PENDING, 'Pending'), (PAYMENT_STATUS_COMPLETE, 'Complete'), (PAYMENT_STATUS_FAILED, 'Failed')]
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_OPTIONS, default=PAYMENT_STATUS_PENDING)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
@@ -57,6 +57,6 @@ class Cart(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
 class CartItem(models.Model):
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, primary_key=True)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField()
