45b9b56dd27c93be6bb7c03f21c0b329529c1d91
David Pecot <4504352+northernocean@users.noreply.github.com>
2022-05-15 11:00:43 -0400

    create Products and Customer models

--- store/models.py
+++ store/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.forms import DateField
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=4000)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=50)
+    birth_date = DateField(null=True)
+
