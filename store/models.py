33a5b0e254ae017a13d537fa6ed2cc77adfa98d1
David Pecot <4504352+northernocean@users.noreply.github.com>
2022-05-16 22:03:46 -0400

    adding index to DB

--- store/models.py
+++ store/models.py
@@ -34,6 +34,11 @@ class Customer(models.Model):
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
+    class Meta:
+        indexes = [
+            models.Index(fields=['last_name', 'first_name'])
+        ]
+
 class Order(models.Model):
     PAYMENT_STATUS_PENDING='P'
     PAYMENT_STATUS_COMPLETE='C'
