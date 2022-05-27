e4aa593fd701562ddbe04e4db2d8d2f1385fd57e
David Pecot <4504352+northernocean@users.noreply.github.com>
2022-05-27 12:08:13 -0400

    testing queries

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from store.models import Product
+from store.models import Customer
+from store.models import Collection
+from store.models import Order
+from store.models import OrderItem
 
 def say_hello(request):
-    query_set = Product.objects.all()
+    a = OrderItem.objects.filter(product__collection__id=3)
 
-    for product in query_set:
-        print(product)
+    for item in a:
+        print(item)
 
     return render(request, 'hello.html', {'name': 'Mosh'})
