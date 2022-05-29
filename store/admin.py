f7908166df30f4531e9009563a894b0757e0e831
David Pecot <4504352+northernocean@users.noreply.github.com>
2022-05-29 11:33:41 -0400

    customizing forms

--- store/admin.py
+++ store/admin.py
@@ -50,6 +50,7 @@ class CustomerAdmin(admin.ModelAdmin):
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
     list_display = ['title', 'products_count']
+    search_fields = ['title']
 
     @admin.display(ordering='products_count')
     def products_count(self, collection):
@@ -71,6 +72,7 @@ class CollectionAdmin(admin.ModelAdmin):
 # ----- 
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['customer']
     list_display = ['id', 'placed_at', 'customer']
     list_per_page = 20
     #list_select_related = ['customer']
@@ -80,6 +82,10 @@ class OrderAdmin(admin.ModelAdmin):
 # -------
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['collection']
+    prepopulated_fields = {
+        'slug': ['title']
+    }
     actions = ['clear_inventory']
     list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
