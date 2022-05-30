973022209d35ecf686a047aabfafac220e730e7f
David Pecot <4504352+northernocean@users.noreply.github.com>
2022-05-30 14:00:57 -0400

    include collection id in products

--- store/serialize.py
+++ store/serialize.py
@@ -2,7 +2,7 @@ from itertools import product
 from math import prod
 from unittest.util import _MAX_LENGTH
 from rest_framework import serializers
-from store.models import Product
+from store.models import Product, Collection
 from decimal import Decimal
 
 class ProductSerializer(serializers.Serializer):
@@ -10,6 +10,9 @@ class ProductSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
     price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+    collection = serializers.PrimaryKeyRelatedField(
+        queryset = Collection.objects.all()
+    )
 
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
