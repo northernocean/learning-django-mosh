e9669cb16c8b9efa93447633bb33534a6a97eba9
David Pecot <4504352+northernocean@users.noreply.github.com>
2022-05-30 14:04:07 -0400

    include collection as nested object

--- store/serialize.py
+++ store/serialize.py
@@ -1,16 +1,21 @@
 from itertools import product
 from math import prod
 from unittest.util import _MAX_LENGTH
+from django.forms import CharField, IntegerField
 from rest_framework import serializers
 from store.models import Product, Collection
 from decimal import Decimal
+
+class CollectionSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
     price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    collection = serializers.StringRelatedField()
+    collection = CollectionSerializer()
 
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
