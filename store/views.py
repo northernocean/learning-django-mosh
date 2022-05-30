d117d35f972360bd5cd6a17809e7c93253ae8c71
David Pecot <4504352+northernocean@users.noreply.github.com>
2022-05-30 14:18:22 -0400

    include collection as hyperlink

--- store/views.py
+++ store/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -9,7 +10,7 @@ from rest_framework import status
 @api_view()
 def product_list(request):
     queryset = Product.objects.select_related('collection').all()
-    serializer = ProductSerializer(queryset, many = True)
+    serializer = ProductSerializer(queryset, many=True, context={'request': request})
     return Response(serializer.data)
 
 
@@ -18,3 +19,8 @@ def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
     serializer = ProductSerializer(product)
     return Response(serializer.data)
+
+@api_view()
+def collection_detail(request, pk):
+    return Response('ok')
+
