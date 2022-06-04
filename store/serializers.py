b4cf5f9456dcb9b0d3db0c8e86551e45079e8e12
David Pecot <4504352+northernocean@users.noreply.github.com>
2022-06-04 14:02:53 -0400

    implement collection list view with generic views

--- store/serializers.py
+++ store/serializers.py
@@ -9,7 +9,9 @@ from decimal import Decimal
 class CollectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Collection
-        fields = ['id', 'title']
+        fields = ['id', 'title', 'products_count']
+    
+    products_count = serializers.IntegerField(read_only=True)
 
 
 class ProductSerializer(serializers.ModelSerializer):
