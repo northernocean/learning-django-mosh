f18b4aed6defcbdf3338dc5a60bfaa048bd8f8b4
David Pecot <4504352+northernocean@users.noreply.github.com>
2022-06-04 13:53:43 -0400

    converting product views to class-based views

--- store/views.py
+++ store/views.py
@@ -7,24 +7,20 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.mixins import ListModelMixin, CreateModelMixin
+from rest_framework.generics import ListCreateAPIView
 from store.models import Product
 from .serializers import ProductSerializer
 from rest_framework import status
 
 
-class ProductList(APIView):
+class ProductList(ListCreateAPIView):
 
-    def get(self, request):
-        queryset = Product.objects.select_related('collection').all()
-        serializer = ProductSerializer(queryset, many=True, context={'request': request})
-        return Response(serializer.data)
+    queryset = Product.objects.select_related('collection').all()
+    serializer_class  = ProductSerializer
 
-    def post(self, request):
-        serializer = ProductSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
+    def get_serializer_context(self):
+        return { 'request': self.request }
+    
 
 class ProductDetail(APIView):
 
