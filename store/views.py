b479029c7b5f01a864a066bfdbddf2e7cf0d0250
David Pecot <4504352+northernocean@users.noreply.github.com>
2022-06-04 16:10:41 -0400

    ditto for collections

--- store/views.py
+++ store/views.py
@@ -20,12 +20,10 @@ class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(products_count=Count('products')).all()
     serializer_class = CollectionSerializer
 
-    def delete(self, request, pk):
-        collection = get_object_or_404(Collection, pk=pk)
-        if collection.products.count() > 0:
-            return Response({'error': 'Collection cannot be deleted since it contains products'})
-        collection.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def destroy(self, request, *args, **kwargs):
+        if Product.objects.filter(collection_id=kwargs['pk']).count() > 0:
+            return Response({'error':'Collection cannot be deleted. It is referenced by existing products'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        return super().destroy(request, *args, **kwargs)
 
 
 class ProductViewSet(ModelViewSet):
