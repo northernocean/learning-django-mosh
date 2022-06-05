1eb12142698412f0506d23776f60601d02de1c88
David Pecot <4504352+northernocean@users.noreply.github.com>
2022-06-04 21:18:00 -0400

    fix error when adding sorting

--- store/views.py
+++ store/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import get_object_or_404
 from django.db.models.aggregates import Count
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from store.models import OrderItem, Product, Collection, Review
@@ -25,11 +24,10 @@ class ProductViewSet(ModelViewSet):
 
     queryset = Product.objects.all()
     serializer_class  = ProductSerializer
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ['collection_id']
     search_fields = ['title', 'description']
     ordering_fields = ['unit_price', 'last_update']
-    pagination_class = PageNumberPagination
     
     def get_serializer_context(self):
         return { 'request': self.request }
