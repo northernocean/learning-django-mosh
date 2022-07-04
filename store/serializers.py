8e903b719c1383ec9609c3fda7f0672d768c83bf
David Pecot <4504352+northernocean@users.noreply.github.com>
2022-07-04 16:06:20 -0400

    signals

--- store/serializers.py
+++ store/serializers.py
@@ -4,7 +4,7 @@ from pyexpat import model
 from django.db import transaction
 from rest_framework import serializers
 from .models import Cart, CartItem, Customer, Order, OrderItem, Product, Collection, Review
-
+from .signals import order_created
 
 class CollectionSerializer(serializers.ModelSerializer):
     class Meta:
@@ -160,5 +160,6 @@ class CreateOrderSerializer(serializers.Serializer):
             ]
             OrderItem.objects.bulk_create(order_items)
             Cart.objects.filter(pk=cart_id).delete()
+            order_created.send_robust(self.__class__, order=order)
             return order
 
