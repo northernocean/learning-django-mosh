8734523c9143d1e221f2887a3f1fdc24e26bbf93
David Pecot <4504352+northernocean@users.noreply.github.com>
2022-07-09 11:24:08 -0400

    sending email with attachments

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from django.core.mail import send_mail, mail_admins, BadHeaderError
+from django.core.mail import send_mail, mail_admins, BadHeaderError, EmailMessage
 
 def say_hello(request):
     try:
-        send_mail('subject', 'message', 'noreply@example.com', ['bob@example.com'])
+        #send_mail('subject', 'message', 'noreply@example.com', ['bob@example.com'])
+        message = EmailMessage('subject', 'message', 'noreply@example.com', ['bob@example.com'])
+        message.attach_file('playground/static/images/turtle.jpeg')
+        message.send()
     except BadHeaderError:
         # send_mail can throw errors if the mail is attempted to be modified (?)
         pass
