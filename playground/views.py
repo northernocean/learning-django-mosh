c5ad639f9f177666e912634385e8a3991fab85cd
David Pecot <4504352+northernocean@users.noreply.github.com>
2022-07-25 19:08:07 -0400

    sending emails with celery

--- playground/views.py
+++ playground/views.py
@@ -1,14 +1,6 @@
 from django.shortcuts import render
-from django.core.mail import send_mail, mail_admins, BadHeaderError, EmailMessage
+from .tasks import notify_customers
 
 def say_hello(request):
-    try:
-        #send_mail('subject', 'message', 'noreply@example.com', ['bob@example.com'])
-        message = EmailMessage('subject', 'message', 'noreply@example.com', ['bob@example.com'])
-        message.attach_file('playground/static/images/turtle.jpeg')
-        message.send()
-    except BadHeaderError:
-        # catching errors if emails are attempted to be "spoofed" - TODO: learn more
-        print("Error: bad headers")
-    # if email was successful - display the hello world page
+    notify_customers.delay('Hello World')
     return render(request, 'hello.html', {'name': 'Mosh'})
