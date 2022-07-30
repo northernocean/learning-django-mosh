66734d92e613d87a37007e6811065c77f61c3a3f
David Pecot <4504352+northernocean@users.noreply.github.com>
2022-07-30 12:10:15 -0400

    first tests

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,6 +1,9 @@
+import django
 from rest_framework.test import APIClient
 from rest_framework import status
+import pytest
 
+@pytest.mark.django_db
 class TestCreateCollection:
 
     def test_user_is_anonymous_returns_404(self):
