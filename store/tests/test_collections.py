c8521f21c9446532f22306bdc9bb688cfa982551
David Pecot <4504352+northernocean@users.noreply.github.com>
2022-07-31 11:03:54 -0400

    first tests

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -7,8 +7,12 @@ import pytest
 class TestCreateCollection:
 
     def test_user_is_anonymous_returns_404(self):
-        
         client = APIClient()
         response = client.post('/store/collections/', { 'title': 'a'})
-
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    
+    def test_user_is_not_admin_returns_403(self):
+        client = APIClient()
+        client.force_authenticate(user={})
+        response = client.post('/store/collections/', { 'title': 'a'})
+        assert response.status_code == status.HTTP_403_FORBIDDEN
