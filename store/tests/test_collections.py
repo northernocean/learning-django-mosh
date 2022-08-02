6bcbec01f39bf35596b08d859efcb18953730570
David Pecot <4504352+northernocean@users.noreply.github.com>
2022-08-01 21:07:04 -0400

    more tests

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,4 +1,5 @@
-import django
+from django.contrib.auth.models import User
+from pyparsing import null_debug_action
 from rest_framework.test import APIClient
 from rest_framework import status
 import pytest
@@ -8,11 +9,25 @@ class TestCreateCollection:
 
     def test_user_is_anonymous_returns_404(self):
         client = APIClient()
-        response = client.post('/store/collections/', { 'title': 'a'})
+        response = client.post('/store/collections/', {'title': 'a'})
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
     
     def test_user_is_not_admin_returns_403(self):
         client = APIClient()
         client.force_authenticate(user={})
-        response = client.post('/store/collections/', { 'title': 'a'})
+        response = client.post('/store/collections/', {'title': 'a'})
         assert response.status_code == status.HTTP_403_FORBIDDEN
+    
+    def test_if_data_is_invalid_returns_400(self):
+        client = APIClient()
+        client.force_authenticate(user=User(is_staff=True))
+        response = client.post('/store/collections/', {'title': ''})
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data['title'] is not None
+
+    def test_if_data_is_valid_returns_201(self):
+        client = APIClient()
+        client.force_authenticate(user=User(is_staff=True))
+        response = client.post('/store/collections/', {'title': 'a'})
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.data['id'] > 0
