90b90b0a95dd69339aa0732824bf8876f15f728b
David Pecot <4504352+northernocean@users.noreply.github.com>
2022-08-20 20:59:11 -0400

    add logging to critical code section

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from .tasks import notify_customers
+import logging
+import requests
+
+logger = logging.getLogger(__name__)
 
 def say_hello(request):
+    try:
+        logger.info('calling httpbin')
+        response = requests.get('https://httpbin.org/delay/2')
+        logger.info('received response from httpbin')
+        data = response.json()
+    except requests.ConnectionError:
+        logger.critical('httpbin is offline')
     notify_customers.delay('Hello World')
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    return render(request, 'hello.html', {'name': data})
